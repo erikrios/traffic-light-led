34d48e6647d4b2f01b58b022a5849f191c58f805
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-05-09 22:04:09 +0700

    Add the sample code of traffic light

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <Arduino.h>
 
+const int LED_RED = 1;
+const int LED_YELLOW = 2;
+const int LED_GREEN = 3;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_RED, HIGH);
+  delay(5000);
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(1000);
+  digitalWrite(LED_RED, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_GREEN, HIGH);
+  delay(5000);
+  digitalWrite(LED_YELLOW, HIGH);
+  delay(1000);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, LOW);
 }
